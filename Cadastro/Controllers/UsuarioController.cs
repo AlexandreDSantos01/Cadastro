8502201760dcadf3e3610fd86c6232222b1d70a1
AlexandreDSantos01 <Alexandreds4@hotmail.com>
2025-01-18 02:13:44 -0300

    Adicionar arquivos de projeto.

---
+++ Cadastro/Controllers/UsuarioController.cs
@@ -0,0 +1,43 @@
+﻿using Cadastro.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+namespace Cadastro.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private static List<Usuario> usuarios = new List<Usuario>();
+
+        public IActionResult Create()
+
+        {
+            ViewBag.Sexos = Enum.GetValues(typeof(Usuario.Sexo));
+            return View();
+        }
+
+        //Processa o cadastro
+        [HttpPost]
+        public IActionResult Create(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                usuario.IdUsuario = usuarios.Count + 1; //simula a criaçaõ de um ID
+                usuarios.Add(usuario);
+                return RedirectToAction("Index");
+            }
+            ViewBag.Sexos = Enum.GetValues(typeof(Usuario.Sexo));
+            return View(usuario);
+        }
+
+        //Lista os usuarios cadastrados
+        public IActionResult Index()
+        {
+            return View(usuarios);
+        }
+
+
+
+
+    }
+}
---
+++ Cadastro/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cadastro.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        [Required(ErrorMessage = " O nome é obrigatório")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "A data de nascimento é obrigatório")]
+        public DateTime DataNasc { get; set; }
+        public enum Sexo
+        {
+            Masculino,
+            Feminino,
+            Outros,
+            NaoInformar
+        }
+        public Sexo TipoSexo { get; set; }
+        [Required(ErrorMessage = "O email é obrigatório")]
+        [EmailAddress(ErrorMessage = "O email não é valido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = " O Endereço é obrigatorio")]
+        public string Endereco { get; set; }
+        [Required(ErrorMessage = "O numero da rua é obrigatorio")]
+        public int NRua { get; set; }
+    }
+}
